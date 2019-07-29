8c264af42b61fa521fbb7f7d9beca2befa2c6193
Josh Wu <josh.wu@tradevan.com.tw>
2019-07-29 09:34:05 +0800

    Updates

---
+++ precache-manifest.304710146d377bd26138a2561e20a186.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21aa3baebf6d1dd39f8196094eb7af79",
+    "url": "/Week2_Freecell/index.html"
+  },
+  {
+    "revision": "951888c549908090e4ed",
+    "url": "/Week2_Freecell/static/css/main.15f16324.chunk.css"
+  },
+  {
+    "revision": "352edc297fc6d2506274",
+    "url": "/Week2_Freecell/static/js/2.a3e86c29.chunk.js"
+  },
+  {
+    "revision": "951888c549908090e4ed",
+    "url": "/Week2_Freecell/static/js/main.34f500e3.chunk.js"
+  },
+  {
+    "revision": "e2deb59523b20c331f97",
+    "url": "/Week2_Freecell/static/js/runtime~main.d220063a.js"
+  },
+  {
+    "revision": "b8c3e933766f7fb0e1eb41cf56b3cbec",
+    "url": "/Week2_Freecell/static/media/freecell_bg.b8c3e933.png"
+  }
+]);
